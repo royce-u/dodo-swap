cb98cca1bdc6f613b8e2b0b75f09dfd40ec14860
essleeung <essleeung@gmail.com>
2020-05-18 17:12:07 -0400

    updated user schema

--- dodoswap-server/models/user.ts
+++ dodoswap-server/models/user.ts
@@ -1,5 +1,9 @@
 import * as mongoose from 'mongoose'
 import * as bcrypt from 'bcryptjs'
+import Rating from '../models/rating'
+import Item from '../models/item'
+import Event from '../models/event'
+
 
 // Create User interface extending mongoose.Document (which includes ._id)
 export default interface User extends mongoose.Document {
@@ -7,7 +11,15 @@ export default interface User extends mongoose.Document {
     lastname: string;
     email: string;
     password: string;
+    userName: string;
+    islandName: string;
+    nativeFruit: string;
     pic: string; 
+    ratings: Rating[];
+    inventory: Item[];
+    wishList: Item[];
+    friends: User[];
+    events: Event[];
     validPassword(user: User, password: string): boolean;
 }
 
@@ -28,8 +40,18 @@ let userSchema: mongoose.Schema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 8
-    },
-    pic: String
+    }, 
+    userName: String,
+    islandName: String,
+    nativeFruit: String,
+    pic: String,
+    ratings: [{type: mongoose.Schema.Types.ObjectId, ref: 'Rating'}],
+    inventory: [{type: mongoose.Schema.Types.ObjectId, ref: 'Item'}],
+    wishList: [{type: mongoose.Schema.Types.ObjectId, ref: 'Item'}],
+    friends: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    events: [{type: mongoose.Schema.Types.ObjectId, ref: 'Event'}]
+
+
 })
 //hash the passwords
 userSchema.pre('save', function(this: User, done) {
