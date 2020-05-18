2f590a602bce97829977cff0366dba2e701595d2
essleeung <essleeung@gmail.com>
2020-05-18 15:38:53 -0400

    made changes to packages2

--- dodoswap-server/controllers/auth.ts
+++ dodoswap-server/controllers/auth.ts
@@ -1,42 +1,54 @@
+//Declare variables and import dependencies
 require('dotenv').config()
 let db = require('../models')
-let router = require('express').Router()
 let jwt = require('jsonwebtoken')
 
+import { Request, Response, Router } from 'express'
+import User from '../models/user'
+
+const router = Router()
+
 // POST /auth/login (find and validate user; send token)
-router.post('/login', (req, res) => {
+router.post('/login', (req: Request, res: Response) => {
+  //Declare email and password variables
+  const email = (req.body as {email: string}).email
+  const password = (req.body as {password: string}).password 
   console.log(req.body)
+
   //Look up the user by email to make sure they are "new"
-  db.User.findOne({ email: req.body. email })
-  .then(user => {
+  db.User.findOne({ email: email })
+  .then((user: User) => {
     //check if user exists
     if (!user) {
       //They don't have an account, send error message
       return res.status(404).send( { message: 'User not found'})
     }
     //They exist - but make sure they have a correct password
-    if (!user.validPassword(req.body.password)) {
+    if (!user.validPassword(user, password)) {
       //Incorrect password, send error back
       return res.status(401).send({ message: 'Invalid credentials'})
     }
     //We have a good user - make them a new token, send it to them
-    let token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+    let token: string = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
       expiresIn: 60 * 60 * 8 //8 hours in seconds
     })
     res.send({token})
   })
-  .catch(err => {
+  .catch((err: any) => {
     console.log('Error in POST /auth/login', err)
     res.status(503).send({ message: 'Server-side or DB error '})
   })
 })
 
 // POST to /auth/signup (create user; generate token)
-router.post('/signup', (req, res) => {
+router.post('/signup', (req: Request, res: Response) => {
   console.log(req.body)
+ //Declare email and password variables
+ const email = (req.body as {email: string}).email
+
   //Look up the user by email to make sure they are "new"
-  db.User.findOne( {email: req.body.email})
-  .then(user => {
+  db.User.findOne( {email: email})
+  .then((user: User) => {
     //if user exists already then do NOT let them create another account
     if (user) {
       //nono! sign in instead
@@ -44,14 +56,14 @@ router.post('/signup', (req, res) => {
     }
     //we know the user is legitimately a new user: CREATE A NEW ACCOUNT
     db.User.create(req.body) 
-      .then(newUser => {
+      .then((newUser: User) => {
         //Yay! Things worked and user exists now. create a token for the new user
-        let token = jwt.sign(newUser.toJSON(), process.env.JWT_SECRET, {
+        let token: string = jwt.sign(newUser.toJSON(), process.env.JWT_SECRET, {
           expiresIn: 120 //60 * 60 * 8 //8 hours, in seconds
         })
         res.send({ token })
       })
-      .catch(innerErr => {
+      .catch((innerErr: any) => {
         console.log('Error creating user', innerErr)
         if (innerErr.name === 'ValidationError') {
           res.status(412).send({ message: `Validation Error!' ${innerErr.message}.`})
@@ -61,7 +73,7 @@ router.post('/signup', (req, res) => {
         }
       })
     })
-  .catch(err => {
+  .catch((err: any) => {
     console.log("ERROR IN POST /auth/signup", err)
     res.status(503).send({ message: 'Database or server error'})
   })
