46f5a8f96b703a0fa9f65a67b96bc3ef2cef635a
essleeung <essleeung@gmail.com>
2020-05-19 00:02:25 -0400

    items json, need to fix seed file

--- dodoswap-server/models/item.ts
+++ dodoswap-server/models/item.ts
@@ -1,21 +1,24 @@
 import * as mongoose from 'mongoose'
+
 //Create Item interface extending mongoose.Document (which includes ._id)
 export default interface Item extends mongoose.Document {
-acID: number;
-name: string;
-variation: string;
-image: string;
-buy: number;
-sell: number
+    acID: number;
+    name: string;
+    variation: string;
+    image: string;
+    buy: number;
+    sell: number;
+    tag: string;
 }
 //create item schema
 let itemSchema: mongoose.Schema = new mongoose.Schema({
-acID: String,
-name: String,
-variation: String,
-image: String,
-buy: Number,
-sell: Number
+    acID: String,
+    name: String,
+    variation: String,
+    image: String,
+    buy: Number,
+    sell: Number, 
+    tag: String
 })
 //create model with type item and export item model
 let Item: mongoose.Model<Item> = mongoose.model<Item>('Item', itemSchema)
