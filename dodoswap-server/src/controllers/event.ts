a7514b25f83823f043c5eb102bf5763aaccd1a96
essleeung <essleeung@gmail.com>
2020-05-26 04:34:45 -0400

    more styling of my inventory and wishlist grids

--- dodoswap-server/src/controllers/event.ts
+++ dodoswap-server/src/controllers/event.ts
@@ -30,8 +30,8 @@ router.get('/', (req: Request, res: Response) => {
 router.get('/:id', (req: Request, res: Response) => {
     console.log("EVENTS ONE-", (req.params as { id: string }).id)
     db.Event.findById((req.params as { id: string }).id)
-        .populate('attendees')
         .populate('hostId')
+        .populate('attendees.attendeeId')
         .then((event: EventInterface | null) => {
             console.log("event returned--", event)
             res.send({ event })
